e84e0f0cf8cf76493e2562ab8ba784bf5c51a931
Patrick Melo <patrickabimael@yahoo.com.br>
2024-10-16 00:18:04 -0300

    💄 feat: Inicia o jogo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgForOf} from "@angular/common";
+import { emptyGrid } from "./assets/assets";
+import { GameState } from './enums/GameState';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'jogo-da-velha';
+  gameState: GameState = GameState.WAITING_START;
+  grid: string[][] = emptyGrid
+  protected readonly GameState = GameState;
+
+  startNewGame() {
+    this.gameState = GameState.X_TURN;
+    this.grid = emptyGrid;
+  }
 }
