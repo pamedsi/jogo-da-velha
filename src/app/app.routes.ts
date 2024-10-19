a4b83afa6ac137e326dd08367c6113e6e7cd2c0c
Patrick Melo <patrickabimael@yahoo.com.br>
2024-10-19 07:04:10 -0300

    ✨ feat: Cria página de rota não encontrada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,16 @@
 import { Routes } from '@angular/router';
+import {PageNotFoundComponent} from "./components/page-not-found/page-not-found.component";
+import {GameScreenComponent} from "./components/game-screen/game-screen.component";
+import {FullSessionComponent} from "./components/full-session/full-session.component";
 
 export const routes: Routes = [
-
-];
+  {
+    path: '',
+    component: GameScreenComponent,
+  },
+  {
+    path: 'error',
+    component: FullSessionComponent,
+  },
+  { path: '**', component: PageNotFoundComponent },
+]
