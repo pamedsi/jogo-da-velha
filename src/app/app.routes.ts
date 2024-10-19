334780a15c78c191aaf29dbe1fe9ea31636c2aa9
Patrick Melo <patrickabimael@yahoo.com.br>
2024-10-19 14:23:26 -0300

    ♻️ refactor: Valida sessão ao iniciar o componente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
-import {PageNotFoundComponent} from "./components/page-not-found/page-not-found.component";
-import {GameScreenComponent} from "./components/game-screen/game-screen.component";
+import {NotFoundComponent} from "./pages/not-found/not-found.component";
+import {GameScreenComponent} from "./pages/game-screen/game-screen.component";
+import {HomeComponent} from "./pages/home/home.component";
 
 export const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'game',
     component: GameScreenComponent,
   },
-  { path: '**', component: PageNotFoundComponent },
+  { path: '**', component: NotFoundComponent },
 ]
