e0b5be6c5994999f37d60d2e0268007c32ca8c39
Patrick Melo <patrickabimael@yahoo.com.br>
2024-10-20 18:52:09 -0300

    ✨ feat: Implementa websocket

--- src/app/services/SessionService.ts
+++ src/app/services/SessionService.ts
@@ -1,5 +1,5 @@
-import {Injectable, NgModule} from "@angular/core";
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
 import {Player} from "../types/Player";
 import {SessionResponse} from "../types/dto/SessionResponse";
 
@@ -7,14 +7,16 @@ import {SessionResponse} from "../types/dto/SessionResponse";
     providedIn: 'root'
 })
 export class SessionService {
+    private readonly endpoint = 'server/session';
+
     constructor(private http: HttpClient) {}
 
     startSession(player: Player) {
-        return this.http.post<SessionResponse>('server/join', {player})
+        return this.http.post<SessionResponse>(`${this.endpoint}/join`, {player})
     }
 
-  validateSession(sessionID: string) {
-      return this.http.get<SessionResponse>(`server/validate/${sessionID}`)
-  }
+    validateSession(sessionID: string) {
+      return this.http.get<SessionResponse>(`${this.endpoint}/validate/${sessionID}`)
+    }
 }
 
--- src/app/services/WebSocketService.ts
+++ src/app/services/WebSocketService.ts
@@ -1,62 +1,33 @@
-import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import {Injectable} from '@angular/core';
+import SockJS from 'sockjs-client';
+import * as Stomp from 'stompjs'
+import {GameEvent} from "../types/GameEvent";
+import {Client} from "stompjs";
+import {Subject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebSocketService {
-  private socket: WebSocket | null = null;
-  private subject: Subject<any> = new Subject();
-  private readonly URL = 'ws://localhost:8080';
+  private readonly url = 'server/websocket'
+  stompClient!: Client
+  onMessageReceived: Subject<GameEvent> = new Subject()
 
-  connect(): void {
-    if (!this.socket || this.socket.readyState === WebSocket.CLOSED) {
-      this.socket = new WebSocket(this.URL);
-      // this.setupSocketEvents();
-    }
+  connect() {
+    const ws = SockJS(this.url)
+    this.stompClient = Stomp.over(ws)
+    this.stompClient.connect(
+      {},
+      () => {
+        this.stompClient.subscribe('/topic/game', (gameEvent: any) => {
+          this.onMessageReceived.next(gameEvent)
+        })
+      },
+      (error) => {
+        console.error("Failed to connect!", error)
+      }
+    )
   }
 
-  disconnect(): void {
-    if (this.socket && this.socket.readyState !== WebSocket.CLOSED) {
-      this.socket.close();
-      this.socket = null;
-    }
-  }
 
-  send(message: any): void {
-    if (this.socket && this.socket.readyState === WebSocket.OPEN) {
-      this.socket.send(JSON.stringify(message));
-    }
-  }
-
-  onMessage(): Observable<any> {
-    return this.subject.asObservable();
-  }
-
-  // private setupSocketEvents(): void {
-  //   this.socket?.onopen = () => {
-  //     console.log('WebSocket connection established');
-  //   };
-  //
-  //   this.socket?.onclose = (event: CloseEvent) => {
-  //     console.log('WebSocket connection closed', event.code, event.reason);
-  //     this.subject.next(null);
-  //   };
-  //
-  //   this.socket?.onmessage = (event: MessageEvent) => {
-  //     try {
-  //       const data = JSON.parse(event.data);
-  //       console.log('Received:', data);
-  //       this.subject.next(data);
-  //     } catch (error) {
-  //       console.error('Failed to parse WebSocket message:', error);
-  //       this.subject.next(null);
-  //     }
-  //   };
-  //
-  //   this.socket?.onerror = (error: Event) => {
-  //     console.error('WebSocket error:', error);
-  //     this.subject.next(null);
-  //   };
-  // }
 }
