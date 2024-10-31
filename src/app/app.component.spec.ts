414c1ddfe96a5fbf3bf3158fcf54be21934e70ff
Patrick Melo <patrickabimael@yahoo.com.br>
2024-10-31 07:44:51 -0300

    🧪 test: Traduz o teste para inglês

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,10 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'jogo-da-velha' title`, () => {
+  it(`should have the 'tic-tac-toe' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('jogo-da-velha');
+    expect(app.title).toEqual('tic-tac-toe');
   });
 
   it('should render title', () => {
